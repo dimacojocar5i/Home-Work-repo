ce878cf8e32e03e4fb13c0241ee12b221df2898e
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-05-25 16:02:55 +0200

    Add new class Exercitiu3.java

---
+++ src/Exercitiu3.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercitiu3 {
+    static void main(String[] args) {
+        List<String> produse = new ArrayList<>(20);
+
+        produse.add("Cipsuri");
+        produse.add("Biscuiti");
+        produse.add("Inghetata");
+        produse.add("Bomboane");
+        produse.add("Apa plata");
+
+       System.out.println("Produsele din cosul de cumparaturi: " + produse);
+        System.out.println();
+
+        produse.remove("Cipsuri");
+        produse.remove("Biscuiti");
+        System.out.println("Cosul fara podusele cu indexul 0 si 1 " + produse);
+        System.out.println();
+
+        produse.set(1,"Fructe");
+        System.out.println("Cosul cu produsele modificate de la indexul 0 si 1 " + produse);
+
+    }
+}
